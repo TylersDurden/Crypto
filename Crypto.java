74d20bf040324cab99ca3d9aa19fa5b2a8c7e8a5
Scott <31575629+TylersDurden@users.noreply.github.com>
2017-10-06 10:35:37 -0400

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -11,7 +11,7 @@ import java.util.*;
  * libraries. 
  * 
  * @Author ScottRobbins
- * 10/3/2017
+ * 10/5/2017
  ****** --- - ---- - ----- ~*CRYPTO*~ ----- - ---- - --- ************/
 public class Crypto {
 
