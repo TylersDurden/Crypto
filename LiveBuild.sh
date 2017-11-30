e2dca6f048c635b5ac1d385f791c98877fd79b92
Scott <31575629+TylersDurden@users.noreply.github.com>
2017-11-30 19:30:12 +0000

    LiveUSB Build
    I need to make this code as portable as possible. Essentially I want to design it so the entire project can be run
    with this script on any Linux machine so long as a USB is plugged in (with the name UNTITLED) and the
    user has an Internet connection.

---
+++ LiveBuild.sh
@@ -0,0 +1,54 @@
+#!/usr/bin
+echo 'Attempting to get Project from git onto USB'
+echo 'From there, Need to compile and run it (all on USB).'
+
+# Create a live running directory on the USB
+cd /media/root/UNTITLED/CRYPTO
+
+# Fetch Crypto Repository from GitHub 
+#git config --global user.name "{}"
+#git config --global user.email "{}"
+#git clone https://www.github.com/TylersDurden/Crypto
+
+#git location: cd /media/root/UNTITLED/CRYPTO/Crypto/src
+
+# Before java Build, put EOWL into txt file 
+# (it will be zipped for portability) 
+cd /media/root/UNTITLED/Crypto2.0/src/DATA
+chmod +x install.sh 
+su root ./install.sh
+# Now put unzipped files into a txt file for reading w Java Apps
+cd /media/root/UNTITLED/CRYPTO/Crypto/src/DATA/Words/EOWL-v1.1.2
+echo 'EOWL unzipped. Relocating to a txt file...'
+cd CSV\ Format
+cat AWords.csv >> words.txt
+cat B\ Words.csv >> words.txt
+cat C\ Words.csv >> words.txt
+cat D\ Words.csv >> words.txt
+cat E\ Words.csv >> words.txt
+cat F\ Words.csv >> words.txt
+cat G\ Words.csv >> words.txt
+cat H\ Words.csv >> words.txt
+cat I\ Words.csv >> words.txt
+cat J\ Words.csv >> words.txt
+cat K\ Words.csv >> words.txt
+cat L\ Words.csv >> words.txt
+cat M\ Words.csv >> words.txt
+cat N\ Words.csv >> words.txt
+cat O\ Words.csv >> words.txt
+cat P\ Words.csv >> words.txt
+cat Q\ Words.csv >> words.txt
+cat R\ Words.csv >> words.txt
+cat S\ Words.csv >> words.txt
+cat T\ Words.csv >> words.txt
+cat U\ Words.csv >> words.txt
+cat V\ Words.csv >> words.txt
+cat W\ Words.csv >> words.txt
+cat X\ Words.csv >> words.txt
+cat Y\ Words.csv >> words.txt
+cat Z\ Words.csv >> words.txt
+
+# Now Build and Run Java Applications 
+cd/media/root/UNTITLED/CRYPTO/Crypto/src/
+chmod +x run.sh
+su root ./run.sh 
